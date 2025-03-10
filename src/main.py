50aef159f6801b9cc1235add4bef54979c912d2c
asilveira-02711 <al.silveira@yahoo.com.br>
2025-03-10 00:30:28 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,11 +9,13 @@ import data_clean
 
 importlib.reload(data_clean)
 
+#source
 df = pd.read_csv(
     "https://raw.githubusercontent.com/JackyP/testing/master/datasets/nycflights.csv",
     index_col=0
     )
 
+#data_clean
 df_raw = data_clean.remove_colunas(df)
 
 df_raw = data_clean.rename_colunas(df_raw)
@@ -21,3 +23,5 @@ df_raw = data_clean.rename_colunas(df_raw)
 df_raw = data_clean.def_type_str(df_raw, ["id_voo"])
 
 df_raw_clean = data_clean.ajusta_campo(df_raw, ["companhia", "id_voo", "origem", "destino"])
+
+#data_transformation
