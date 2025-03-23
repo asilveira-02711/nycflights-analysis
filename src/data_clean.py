228ff6c62a83a3231003748c1ed38aca20160b44
asilveira-02711 <al.silveira@yahoo.com.br>
2025-03-23 11:48:55 -0300

    Atualizacao atividade

--- src/data_clean.py
+++ src/data_clean.py
@@ -5,13 +5,20 @@ import numpy as np
 import re
 import logging
 
-def remove_colunas(df):
+
+def valida_colunas(df, colunas):
     
-    usecols=["year", "month",  "day", "hour", "minute","arr_delay","carrier","flight","air_time","distance", "origin", "dest"]
+    usecols = colunas
     
     if not set(usecols).issubset(set(df.columns)):
         logger.error(f"Mudança de schema; {datetime.datetime.now()} ")
         raise Exception("Mudança de schema")
+    
+    return df
+
+def remove_notnull_duplicates(df, colunas):
+
+    usecols = colunas
 
     df_raw = df.loc[:, usecols].copy()
     df_raw = df_raw[df_raw["air_time"]>0]
@@ -25,18 +32,11 @@ def remove_colunas(df):
 
     return df_raw
 
-#inserir coluna
+def rename_colunas(df, colunas_de, colunas_para):
 
-def rename_colunas(df):
+    usecols2 = colunas_de
 
-    if df is None or df.empty:
-        raise ValueError("DataFrame vazio ou inválido recebido na função rename_colunas")
-    
-    df["date_time"] =  pd.to_datetime(df[["year", "month", "day", "hour", "minute"]],  dayfirst=True)
-
-    usecols2 =["date_time", "arr_delay","carrier","flight","air_time","distance", "origin", "dest" ]
-
-    new_columns = ["data_hora", "atraso_chegada", "companhia", "id_voo","tempo_voo", "distancia", "origem", "destino"]
+    new_columns = colunas_para
 
     columns_map = {usecols2[i]: new_columns[i] for i in range(len(usecols2))}
 
--- src/main.py
+++ src/main.py
@@ -25,17 +25,21 @@ df = pd.read_csv(
     index_col=0
     )
 
-df_raw = data_clean.remove_colunas(df)
+df_raw = data_clean.valida_colunas(df, ["year", "month",  "day", "hour", "minute","arr_delay","carrier","flight","air_time","distance", "origin", "dest"])
 
-df_raw = data_clean.rename_colunas(df_raw)
+df_raw = data_clean.remove_notnull_duplicates(df, ["year", "month",  "day", "hour", "minute","arr_delay","carrier","flight","air_time","distance", "origin", "dest"])
+
+df_raw["date_time"] =  pd.to_datetime(df[["year", "month", "day", "hour", "minute"]],  dayfirst=True)
+
+df_raw = data_clean.rename_colunas(df_raw, ["date_time", "arr_delay","carrier","flight","air_time","distance", "origin", "dest" ], ["data_hora", "atraso_chegada", "companhia", "id_voo","tempo_voo", "distancia", "origem", "destino"])
 
 df_raw = data_clean.def_type_str(df_raw, ["id_voo"])
 
 df_raw_clean = data_clean.ajusta_campo(df_raw, ["companhia", "id_voo", "origem", "destino"])
 
-df_raw_horas_voo = tempo_voo_horas(df_raw_clean, "tempo_voo")
+df_raw_horas_voo = transform.tempo_voo_horas(df_raw_clean, "tempo_voo")
 
-df_transformed = turno_partida(df_raw_horas_voo, "data_hora")
+df_transformed = transform.turno_partida(df_raw_horas_voo, "data_hora")
 
 con = sqlite3.connect('projeto_python.db')
 cur = con.cursor()
